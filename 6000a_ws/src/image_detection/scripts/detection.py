7fb535dcd8d687a72c3d227098cd9672b2160b8f
simba.wang <simba1986.wang@gmail.com>
2023-05-11 00:10:44 +0800

    [ENH] marker send OK.

--- 6000a_ws/src/image_detection/scripts/detection.py
+++ 6000a_ws/src/image_detection/scripts/detection.py
@@ -22,11 +22,38 @@ from cv_bridge import CvBridge, CvBridgeError
 # OpenCV2 for saving an image
 import cv2
 
+
+from visualization_msgs.msg import Marker
+
+def send_a_marker(publisher):
+    marker = Marker()
+    marker.header.frame_id = "base_link"  #!!!!!!!!!!!!!! should check frame name.
+    marker.header.stamp = rospy.Time.now()
+    marker.type = marker.SPHERE
+    marker.action = marker.ADD
+    marker.scale.x = 1
+    marker.scale.y = 1
+    marker.scale.z = 1
+    marker.color.a = 1.0  # Don't forget to set the alpha!
+    marker.color.r = 0.0
+    marker.color.g = 1.0
+    marker.color.b = 0.0
+    marker.pose.orientation.w = 1.0
+    marker.pose.position.x = 1
+    marker.pose.position.y = 1
+    marker.pose.position.z = 1
+    publisher.publish(marker)
+
+# Create a new publisher. We publish messages of type Marker to the topic /visualization_marker
+pub = rospy.Publisher('/visualization_marker', Marker, queue_size=10)
+
+# send_a_marker(pub)
 # Instantiate CvBridge
 bridge = CvBridge()
 def callback(data):
     # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.step)
     log_d(f'recv img {data.header.seq}')
+    send_a_marker(pub)
     try:
         # Convert your ROS Image message to OpenCV2
         cv2_img = bridge.imgmsg_to_cv2(data, "bgr8")
