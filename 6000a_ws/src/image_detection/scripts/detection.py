e87656c610e33a0f22f6445d76929499a91e1b7a
simba.wang <simba1986.wang@gmail.com>
2023-05-10 20:46:23 +0800

    [ENH] can recv the image

--- 6000a_ws/src/image_detection/scripts/detection.py
+++ 6000a_ws/src/image_detection/scripts/detection.py
@@ -1,1 +1,21 @@
-print('hello world')
+import rospy
+from sensor_msgs.msg import Image
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.step)
+
+def listener():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('img_detection', anonymous=True)
+
+    rospy.Subscriber("/vrep/image", Image, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
